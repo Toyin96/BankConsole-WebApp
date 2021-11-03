8de825e5551250ce20e801ca89608715fc13b3c1
Toyin96 <onagoruwam@gmail.com>
2021-11-03 02:27:39 +0100

    added the complete transaction class details

--- Bank Console App/Bank Console App/Program.cs
+++ Bank Console App/Bank Console App/Program.cs
@@ -13,11 +13,49 @@ namespace Bank_Console_App
     class Transaction
     {
         public string transactionId { get; set; }
-        public string date { get; }
+        public DateTime date { get; }
+        public string description { get; set; }
         public string type { get; set; }
-        public string amount { get; set; }
-        public string balance { get; set; }
+        public decimal amount { get; set; }
+        public decimal balance { get; set; }
 
-        void 
+        public Transaction(string transactionId, DateTime transactionDate, string description,
+            string transactionType, string amount, string balance){
+            this.transactionId = transactionId;
+            this.date = transactionDate;
+            this.description = description;
+            this.type = transactionType;
+            this.amount = amount;
+            this.balance = balance;
+        }
+
+        string PerformTransaction(decimal amount){
+            if (amount < this.balance){
+                this.balance -= amount;
+                return "success";
+            }else{
+                return "invalid transaction";
+            }
+        }
+
+        string GetTransactionID(){
+            return this.transactionId;
+        }
+
+        DateTime GetTransactionDate(){
+            return this.date;
+        }
+
+        void SetTransactionType(string typeName){
+            this.type = typeName;
+        }
+
+        DateTime GetTransactionBalance(){
+            return this.balance;
+        }
+
+        ~Transaction(){
+        
+        }
     }
 }

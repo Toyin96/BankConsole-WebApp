a7596c1d027991e382b24630f130bf080d6af778
Toyin96 <onagoruwam@gmail.com>
2021-11-03 02:10:34 +0100

    added the banking class

---
+++ Bank Console App/Bank Console App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bank_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Transaction
+    {
+        public string transactionId { get; set; }
+        public string date { get; }
+        public string type { get; set; }
+        public string amount { get; set; }
+        public string balance { get; set; }
+
+        void 
+    }
+}
